152e18401e9d8c51a79f95b7a81eaa13a34180be
robda20188 <danielrouco188rr@gmail.com>
2024-11-23 12:52:18 +0100

    Adding align function

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <unistd.h> //for sbreak() function
 
-void *malloc(int size);
+#define MACHINE_BITS 64
+#define MACHINE_BYTES MACHINE_BITS / 8
+
+void *mall(int size);
 
 int align(int size);
 
 int main(){
+
+}
+
+void *mall(int size){
 	
 }
 
-void *malloc(int size){
-	
+int align(int size){
+    for(int i = 1; ; i++){
+        if(MACHINE_BYTES * i >= size){
+            return MACHINE_BYTES * i;
+        }
+    }
 }
